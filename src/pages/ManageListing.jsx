378d210349b9e96314fcf2d8cf05fd2dada1c2d8
bhaskar6211 <bhaskarsah63@gmail.com>
2026-02-09 23:45:25 +0530

    start Work for manageListing page

--- src/pages/ManageListing.jsx
+++ src/pages/ManageListing.jsx
@@ -1,6 +1,58 @@
-import React from 'react'
+import React, { use, useState } from 'react'
+import toast from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom'
 
 const ManageListing = () => {
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const {userListings }= useSelector((state)=>state.listing)
+
+  const [loadingListing, setLoadingListing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [formData, setFormData] = useState({
+    title: '',
+    platform: '',
+    username: '',
+    followers_count: '',
+    engagement_rate: '',
+    monthly_views: '',
+    niche: '',
+    price: '',
+    description: '',
+    verified: false,
+    monetized: false,
+    country: '',
+    age_range: '',
+    images: [],
+  })
+
+  const platforms = ['youtube', 'instagram', 'tiktok', 'facebook', 'twitter', 'linkedin', 'pinterest', 'snapchat', 'twitch', 'discord'];
+
+  const niches = ['lifestyle', 'fitness', 'food', 'travel', 'tech', 'gaming', 'fashion', 'beauty', 'business', 'education', 'entertainment', 'music', 'art', 'sports', 'health', 'finance', 'other'];
+
+  const ageRanges = ['13-17 years' , '18-24 years', '25-34 years' , '35-44 years', '45-54 years', '55+ years', 'Mixed ages' ];
+
+ const handleInputChange = (field, value) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+ }
+
+ const handleImageUpload = async (event) =>{
+    const files = Array. from(event.target.files);
+    if (!files.length) return;
+    if(files.length + formData.images.length > 5) return toast.error("You can add up to 5 images")
+
+    setFormData((prev) => ({ ... prev, images: [ ... prev.images, ... files] }))  
+  }
+
+  const removeImage = (indexToRemove) =>{
+    setFormData(()=>({
+
+    }))
+}
+
+
   return (
     <div>
       <h1>ManageListing page</h1>
