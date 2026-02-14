5e990d87cfd92f9c3a5439a61b71b50438e7c094
bhaskar6211 <bhaskarsah63@gmail.com>
2026-02-15 00:29:48 +0530

    remove

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { BoxIcon, Grip, GripIcon, ListIcon, MenuIcon, MessageCircleIcon, XIcon } from 'lucide-react'
+import { useClerk, useUser, UserButton } from '@clerk/clerk-react'
 
 const Navbar = () => {
+
+    const {user} = useUser()
+    const {openSignIn} = useClerk()
 
     const [menuOpen, setMenuOpen] = React.useState(false)
     const navigate = useNavigate()
@@ -14,27 +19,47 @@ const Navbar = () => {
 
                     {/* Desktop Menu */}
                     <div className='hidden sm:flex items-center gap-4 md:gap-8 max-md:text-sm text-gray-800'>
-                        <a href="#" onClick={() => scrollTo(0, 0)}> Home </a>
-                        <a href="#" onClick={() => scrollTo(0, 0)}> Products </a>
-                        <a href="#" onClick={() => scrollTo(0, 0)}> About </a>
-                        <a href="#" onClick={() => scrollTo(0, 0)}> Contact </a>
+                        <Link to="/" onClick={() => scrollTo(0, 0)}> Home </Link>
+                        <Link to="/marketplace" onClick={() => scrollTo(0, 0)}> Marketplace </Link>
+                        <Link to={user ? "/messages" : "#"} onClick={() => user ? scrollTo(0, 0) : openSignIn()}> Messages </Link>
+                        <Link to={user ? "/my-listings" : "#"} onClick={() => user ? scrollTo(0, 0) : openSignIn()}> My Listings </Link>
+
                     </div>
 
-                    <div>
-                        <button className='max-sm:hidden cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
-                        <svg onClick={() => setMenuOpen(true)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="sm:hidden"><path d="M4 5h16" /><path d="M4 12h16" /><path d="M4 19h16" /></svg>
-                    </div>
+                    {!user ? (
+                        <div>
+                            <button onClick={openSignIn} className='max-sm:hidden cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
+                            <MenuIcon onClick={() => setMenuOpen(true)} className='sm:hidden' />
+                        </div>
+                    ) : (
+                        <UserButton>
+                            <UserButton.MenuItems>
+                                <UserButton.Action label='Marketplace' labelIcon={<GripIcon size={16} />} onClick={() => navigate('/marketplace')} />
+                            </UserButton.MenuItems>
+                            <UserButton.MenuItems>
+                                <UserButton.Action label='Messages' labelIcon={<MessageCircleIcon size={16} />} onClick={() => navigate('/messages')} />
+                            </UserButton.MenuItems>
+                            <UserButton.MenuItems>
+                                <UserButton.Action label='My Listings' labelIcon={<ListIcon size={16} />} onClick={() => navigate('/my-listings')} />
+                            </UserButton.MenuItems>
+                            <UserButton.MenuItems>
+                                <UserButton.Action label='My Orders' labelIcon={<BoxIcon size={16} />} onClick={() => navigate('/my-orders')} />
+                            </UserButton.MenuItems>
+                        </UserButton>
+                    )}
+                    
 
                 </div>
                 {/* Mobile Menu */}
                 <div className={`sm:hidden fixed inset-0 ${menuOpen ? 'w-full' : 'w-0'} overflow-hidden bg-white backdrop-blur shadow-xl rounded-lg z-[200] text-sm transition-all`}>
                     <div className='flex flex-col items-center justify-center h-full text-xl font-semibold gap-6 p-4'>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Home </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Products </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> About </a>
-                        <a href='#' onClick={() => scrollTo(0, 0)}> Contact </a>
-                        <button className=' cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
-                        <svg onClick={() => setMenuOpen(false)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="absolute size-8 right-6 top-6 text-gray-500 hover:text-gray-700 cursor-pointer"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
+                        <Link to="/" onClick={() => setMenuOpen(false)}> Home </Link>
+                        <Link to="/marketplace" onClick={() => setMenuOpen(false)}> Marketplace </Link>
+                        <Link to="/messages" onClick={() => user ? setMenuOpen(false) : openSignIn()}> Messages </Link>
+                        <Link to="/my-listings" onClick={() => user ? setMenuOpen(false) : openSignIn()}> My Listings </Link>
+
+                        <button onClick={openSignIn} className=' cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
+                        <XIcon onClick={() => setMenuOpen(false)} className='absolute size-8 top-6 right-6 text-gray-500 hover:text-gray-700 cursor-pointer' />
                     </div>
                 </div>
             </nav>
--- client/src/pages/Marketplace.jsx
+++ client/src/pages/Marketplace.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import { ArrowLeftIcon, FilterIcon, Verified } from 'lucide-react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import ListingCard from '../components/ListingCard'
+import { min } from '@tensorflow/tfjs-core'
+import FilterSidebar from '../components/FilterSidebar'
 
 const Marketplace = () => {
+
+  const [searchParams] = useSearchParams()
+  const search = searchParams.get('search') 
+  const navigate = useNavigate()
+  const [showFilterPhone, setShowFilterPhone] = useState(false)
+  const [filters, setFilters] = useState({
+    platform: null,
+    maxPrice: 100000,
+    minFollowers: 0,
+    niche: null,
+    Verified: false,
+    monetized: false,
+  })
+
+  const {listings} = useSelector(state => state.listing)
+
+  const filteredListings = listings.filter((listing) => {
+    if(filters.platform && filters.platform.length > 0){
+      if(!filters.platform.includes(listing.platform)) return false
+    }
+
+    if(filters.maxPrice){
+      if(listing.price > filters.maxPrice) return false
+    }
+
+    if(filters.minFollowers){
+      if(listing.followers_count < filters.minFollowers) return false
+    }
+
+    if(filters.niche && filters.niche.length > 0){
+      if(!filters.niche.includes(listing.niche)) return false
+    }
+
+    if(filters.verified && listing.verified !== filters.verified) return false
+
+    if(filters.monetized && listing.monetized !== filters.monetized) return false
+
+    if(search){
+      const trimed = search.trim();
+      if(
+        !listing.title.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.username.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.description.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.platform.toLowerCase().includes(trimed.toLowerCase()) &&
+        !listing.niche.toLowerCase().includes(trimed.toLowerCase())
+      )
+      return false
+    }
+    
+
+    return true
+  })
+
+
   return (
-    <div>
-      <h1>Marketplace page</h1>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+
+      <div className='flex items-center justify-between text-slate-500'>
+        <button onClick={()=>{navigate('/'); scrollTo(0,0)}} className='flex items-center gap-2 py-5 cursor-pointer'>
+          <ArrowLeftIcon className='size-4'/>
+          Back to Home
+        </button>
+        <button onClick={()=>setShowFilterPhone(true)} className='flex sm:hidden items-center gap-2 py-5 cursor-pointer'>
+          <FilterIcon className='size-4'/>
+          Filters
+        </button>
+      </div>
+
+      <div className='relative flex items-start justify-between gap-8 pb-8'>
+
+        <FilterSidebar setFilters={setFilters} filters={filters} setShowFilterPhone={setShowFilterPhone} showFilterPhone={showFilterPhone} />
+        <div className='flex-1 grid xl:grid-cols-2 gap-4'>
+          {filteredListings.sort((a, b)=>a.featured ? -1 : b.featured ? 1 :0).map((listing, index)=>(
+            <ListingCard listing={listing} key={index} />
+          )) }
+        </div>
+      </div>
     </div>
   )
 }
