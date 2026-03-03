adec7370edd489536e518b4bd6a4fb1d68e260bc
bhaskar6211 <bhaskarsah63@gmail.com>
2026-03-03 22:30:26 +0530

    create the simple backend server and install dependencies

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-// Main file of Backend
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => res.send("Server is live!"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
