dab0a51d8905dcaa2ec9deaa991335b4a18b6819
bhaskar6211 <bhaskarsah63@gmail.com>
2026-03-17 22:47:56 +0530

    Integrate clerk Auth in Backend

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import prisma from './lib/prisma.js';
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 
 app.get('/', (req, res) => res.send("Server is live!"));
