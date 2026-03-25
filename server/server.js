3b5655dd427bfc04f092a340f7a7e3c989bada7e
bhaskar6211 <bhaskarsah63@gmail.com>
2026-03-25 23:08:03 +0530

    Start integrating inngest for background jobs

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import "dotenv/config";
 import cors from 'cors';
 import prisma from './lib/prisma.js';
 import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+import { inngest, functions } from './inngest/index.js';
 
 const app = express();
 
@@ -12,6 +14,8 @@ app.use(clerkMiddleware());
 
 
 app.get('/', (req, res) => res.send("Server is live!"));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 3000;
 
